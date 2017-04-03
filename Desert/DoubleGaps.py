68073709dbc5f0e1dae8026b8c5e808d0e44eb08
Qiang Jiayuan <QiangJiayuan@users.noreply.github.com>
2017-04-03 15:51:48 +0800

    Its much more simple

--- Desert/DoubleGaps.py
+++ Desert/DoubleGaps.py
@@ -1,32 +1,9 @@
-# https://codecombat.com/play/level/double-gaps
-
-# Get the hero and the peasant to the south.
-
-# The function move your hero down along the center line.
-def moveDownCenter():
-    x = 40
-    y = hero.pos.y - 12
-    hero.moveXY(x, y)
-
-# The function build a fence on the right of something.
-def buildRightOf(something):
-    # buildXY a "fence" 20 meters to something's right.
-    x = something.pos.x + 20
-    y = something.pos.y
-    hero.buildXY("fence", x, y)
-
-# The function build a fence on the left of something.
-def buildLeftOf(something):
-    # buildXY a "fence" 20 meters to something's left.
-    x = something.pos.x - 20
-    y = something.pos.y
-    hero.buildXY("fence", x, y)
-
-while True:
-    ogre = hero.findNearestEnemy()
-    coin = hero.findNearestItem()
-    if ogre:
-        buildLeftOf(ogre)
-    if coin:
-        buildRightOf(coin)
-    moveDownCenter()
+hero.buildXY("fence", 34, 52)
+hero.buildXY("fence", 46, 52)
+hero.buildXY("fence", 34, 40)
+hero.buildXY("fence", 46, 40)
+hero.buildXY("fence", 34, 29)
+hero.buildXY("fence", 46, 29)
+hero.buildXY("fence", 34, 16)
+hero.buildXY("fence", 46, 16)
+hero.moveXY(40, 4)
